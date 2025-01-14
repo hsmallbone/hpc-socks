2f2798d8856effa0b5fcaf512fe556aa1e868539
hsmallbone <hsmallbone@users.noreply.github.com>
2025-01-14 16:19:24 +0800

    Add some checks for previous ssh instances

--- ssh_generator.sh
+++ ssh_generator.sh
@@ -12,6 +12,13 @@ if ! test -f "$HOME/.ssh/config" || grep -qF "remote-socks-node" "$HOME/.ssh/con
       ProxyJump $PROXY_JUMP
   EOF
 fi
+prev=$(ps -u $USER au |grep "[s]sh -f" | tr '\t' ' ' | cut -f3 -d' ')
+if [ -n "$prev" ]; then
+    echo 'Previous ssh instances found with following PIDs:'
+    echo $prev
+    echo 'Suggest removing these with kill if the following processes match:'
+    echo $(ps -u $USER au | grep "[s]sh -f")
+fi
 sed -i -E s"/Hostname.*/Hostname $NODE/" ~/.ssh/config
 echo "Rewritten socks host to $HOST"
 ssh -f -N -L $APP_PORT:localhost:$APP_PORT remote-socks-node
